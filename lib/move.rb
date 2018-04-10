417f7169f6319549a955352151b4c8307b6627c7
Mehwish Aziz <mehwisha@msn.com>
2018-04-10 22:50:03 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,8 +6,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 def input_to_index(user_input)
-  user_input.to_i
-  user_input - 1
+  user_input.to_i - 1
+  
 end
 
 def move(array, index, value = 'X')
